8fb0a7fab65aa0c1c9c46df9acd421f7d604a894
Lincoln Hoey Moore <johlinco@gmail.com>
2024-07-31 13:54:23 -0400

    complete part 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 
 const Statistics = (props) => {
 
+  console.log(
+    props
+  )
   if (props.all === 0) {
     return (
       <div>
@@ -15,15 +18,28 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.all}</p>
-      <p>average {(props.good - props.bad) / props.all}</p>
-      <p>positive {(props.good / props.all) * 100}%</p>
+      <StatisticLine text='good' value={props.good} />
+      <StatisticLine text='neutral' value={props.neutral} />
+      <StatisticLine text='bad' value={props.bad} />
+      <StatisticLine text='all' value={props.all} />
+      <StatisticLine text='average' value={props.average} />
+      <StatisticLine text='positive' value={props.positive} />
     </div>
   )
+}
 
+const StatisticLine = (props) => {
+  return (
+    <p>{props.text} {props.value}{props.text === "positive" ? "%" : ""}</p>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick} >
+      {props.text}
+    </button>
+  )
 }
 
 const App = () => {
@@ -32,19 +48,28 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
   
   const goodClick = () => {
     setGood(good + 1)
     setAll(all + 1)
+    setAverage((good - bad)/all)
+    setPositive((good/all)*100)
   }
 
   const badClick = () => {
     setBad(bad + 1)
     setAll(all + 1)
+    setAverage((good - bad)/all)
+    setPositive((good/all)*100)
   }
 
   const neutralClick = () => {
+    setNeutral(neutral + 1)
     setAll(all + 1)
+    setAverage((good - bad)/all)
+    setPositive((good/all)*100)
   }
 
 
@@ -52,10 +77,10 @@ const App = () => {
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={goodClick}>good</button>
-      <button onClick={neutralClick}>neutral</button>
-      <button onClick={badClick}>bad</button>
-      <Statistics good={good} bad={bad} neutral={neutral} all={all} />
+      <Button handleClick={goodClick} text='good' />
+      <Button handleClick={neutralClick} text='neutral' />
+      <Button handleClick={badClick} text='bad' />
+      <Statistics good={good} bad={bad} neutral={neutral} all={all} average={average} positive={positive} />
     </div>
   )
 }
