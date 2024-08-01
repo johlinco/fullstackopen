3620f41b47632d83f4be06643d91c8726457fe95
Lincoln Hoey Moore <johlinco@gmail.com>
2024-08-01 06:50:06 -0400

    completed part 2.1

--- part2/courseinfo/components/Course.jsx
+++ part2/courseinfo/components/Course.jsx
@@ -1,20 +1,20 @@
 const Course = (props) => {
     console.log(props.course.name)
-    // const Header = (props) => {
-    //   console.log("header props", props)
-    //   return (
-    //     <h1>{props.name}</h1>
-    //   )
-    // }
+    const Header = (props) => {
+      console.log("header props", props)
+      return (
+        <h1>{props.course.name}</h1>
+      )
+    }
     
-    // const Part = (props) => {
-    //   console.log("part props", props)
-    //   return (
-    //     <p>
-    //       {props.part.name} {props.part.exercises}
-    //     </p>
-    //   )
-    // }
+    const Part = (props) => {
+      console.log("part props", props)
+      return (
+        <p>
+          {props.part.name} {props.part.exercises}
+        </p>
+      )
+    }
     
     // const Total = (props) => {
     //   console.log("Total Props", props)
@@ -24,24 +24,23 @@ const Course = (props) => {
     // }
   
   
-    // const Content = (props) => {
-    //   console.log("Content Props", props)
-    //   return (
-    //     <div>
-    //       <Part part={props.course.parts[0]} exercise={props.course.parts[0]} />
-    //       <Part part={props.course.parts[1]} exercise={props.course.parts[1]} />
-    //       <Part part={props.course.parts[2]} exercise={props.course.parts[2]} />
-    //     </div>
-    //   )
-    // }
+    const Content = (props) => {
+      console.log("Content Props", props)
+      return (
+        <div>
+          <Part part={props.course.parts[0]} exercise={props.course.parts[0]} />
+          <Part part={props.course.parts[1]} exercise={props.course.parts[1]} />
+          <Part part={props.course.parts[2]} exercise={props.course.parts[2]} />
+        </div>
+      )
+    }
   
   
     return (
       <div>
-        {/* <Header course={course} />
-        <Content course={course}/>
-        <Total course={course}/> */}
-        <h1>{props.course.name}</h1>
+        <Header course={props.course} />
+        <Content course={props.course}/>
+        {/* <Total course={course}/> */}
       </div>
     )
   }
