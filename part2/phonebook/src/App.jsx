6e94530c2692b6dfa347babcff71cb1b6b43fd0c
Lincoln Hoey Moore <johlinco@gmail.com>
2024-08-02 11:22:14 -0400

    Completed 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,56 @@
 import { useState } from 'react'
 
+const NameDisplay = ({ name }) => {
+  return (
+    <p>{name}</p>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const nameUpdate = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addNewName = (event) => {
+    event.preventDefault()
+    if (newName.length > 0) {
+      let newPersons = [...persons]
+      let newNameObject = { name: newName }
+      newPersons.push(newNameObject)
+      setPersons(newPersons)
+      setNewName('')
+      }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: <input />
+          name: 
+          <input 
+            onChange={nameUpdate} 
+            value={newName}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {
+          persons.map(person => 
+            <NameDisplay key={person.name} name={person.name} />
+          )
+        }
+      </ul>
     </div>
   )
 }
