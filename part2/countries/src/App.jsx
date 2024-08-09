3a56eed78311f7cf4f7692b253a7db102570743b
Lincoln Hoey Moore <johlinco@gmail.com>
2024-08-09 12:25:54 -0400

    Working on 2.19

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -9,23 +9,39 @@ const LanguageToDisplay = ({ language }) => {
   )
 }
 
-const Display = ({ countryList }) => {
-
-  if (countryList.length === 1) {
-    const filteredLangages = Object.values(countryList[0].languages)
-    return (
-      <div>
-        <h1>{countryList[0].name.common}</h1>
-        <p>capital {countryList[0].capital}</p>
-        <p>area {countryList[0].area}</p>
+const CountryView =({ languageList, commonName, capital, area, flagUrl, flagAlt }) => {
+  return (
+    <div>
+        <h1>{commonName}</h1>
+        <p>capital {capital}</p>
+        <p>area {area}</p>
         <p><strong>languages</strong></p>
         <ul>
-          {filteredLangages.map(language => 
-            <LanguageToDisplay language={language} />  
+          {languageList.map(language => 
+            <LanguageToDisplay key={language} language={language} />  
           )}
 
         </ul>
-        <img src={countryList[0].flags.png} alt={countryList[0].flags.alt} ></img>
+        <img src={flagUrl} alt={flagAlt} ></img>
+      </div>
+  )
+}
+
+
+const Display = ({ countryList }) => {
+
+  if (countryList.length === 1) {
+    const filteredLanguages = Object.values(countryList[0].languages)
+    return (
+      <div>
+        <CountryView 
+          languageList={filteredLanguages} 
+          commonName={countryList[0].name.common}
+          capital={countryList[0].capital}
+          area={countryList[0].area}
+          flagUrl={countryList[0].flags.png}
+          flagAlt={countryList[0].flags.alt}
+        />
       </div>
     )
   } else if (countryList.length < 10) {
@@ -34,7 +50,14 @@ const Display = ({ countryList }) => {
         <ul>
           {
             countryList.map(country =>
-              <p>{country.name.common}</p>
+              <div>
+                <p key={country.name.common}>{country.name.common}</p>
+                <button 
+                  key={country.name.commom}
+                >
+                  Show {country.name.common}
+                </button>
+              </div>
             )
           }
         </ul>
@@ -76,8 +99,13 @@ function App() {
 
   return (
     <>
-      <Filter searchText={searchText} searchTextUpdate={searchTextUpdate} />
-      <Display countryList={countriesToShow}/>
+      <Filter 
+        searchText={searchText} 
+        searchTextUpdate={searchTextUpdate} 
+      />
+      <Display 
+        countryList={countriesToShow} 
+      />
     </>
   )
 }
