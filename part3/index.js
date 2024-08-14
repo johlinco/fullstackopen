c26c82f9e58481899c5b42f03137a8180561eacf
Lincoln Hoey Moore <johlinco@gmail.com>
2024-08-14 06:01:31 -0400

    Finalizing first portion of course materials

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+
 
 let notes = [
   {
@@ -27,6 +30,51 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+  })
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id))) 
+    : 0
+
+    return String(maxId + 1)
+  }
+
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.data(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        importatnt: Boolean(body.important) || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+  })
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
