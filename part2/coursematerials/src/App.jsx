9edb47aaa9bda80a57ddb25c01cf10bf8a268b1a
Lincoln Hoey Moore <johlinco@gmail.com>
2024-09-05 10:25:22 -0400

    finishing part 3C coursematerials

--- part2/coursematerials/src/App.jsx
+++ part2/coursematerials/src/App.jsx
@@ -58,26 +58,26 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
-  // const toggleImportanceOf = id => {
-  //   const url = `http://localhost:3001/notes/${id}`
-  //   const note = notes.find(n => n.id === id)
-  //   const changedNote = { ...note, important: !note.important }
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
   
-  //   noteService
-  //     .update(id, changedNote)
-  //     .then(response => {
-  //       setNotes(notes.map(note => note.id !== id ? note : response))
-  //     })
-  //     .catch(error => {
-  //       setErrorMessage(
-  //         `Note '${errorMessage} was already removed from the server.` 
-  //       )
-  //       setTimeout(() => {
-  //         setErrorMessage(null)
-  //       }, 5000)
-  //       setNotes(notes.filter(n => n.id !== id))
-  //     })
-  // }
+    noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${errorMessage} was already removed from the server.` 
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
 
   return (
     <div>
--- part3/index.js
+++ part3/index.js
@@ -97,20 +97,52 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, respons, next) => {
+  const body = request.body
 
-  response.status(204).end()
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      respons.json(updatedNote)
+    })
+    .catch(error => {next(error)})
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
