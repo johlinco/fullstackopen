71160ca78489567511bacd1f9aaaec786be5d03e
Lincoln Hoey Moore <johlinco@gmail.com>
2024-09-07 06:15:27 -0400

    completing part 4.3

--- part4/tests/average.test.js
+++ part4/tests/average.test.js
@@ -1,23 +1,21 @@
 
-const { test, describe } = require('node:test')
-const { assert } = require('node:assert')
+// const { test, describe } = require('node:test')
+// const { assert } = require('node:assert')
 
-// ...
+// const average = require('../utils/for_testing').average
 
-const average = require('../utils/for_testing').average
-
-describe('average', () => {
-    test('of one value is the value itself', () => {
-        const result = average([1])
-        console.log(result)
-      assert.strictEqual(result, 1)
-    })
+// describe('average', () => {
+//     test('of one value is the value itself', () => {
+//         const result = average([1])
+//         console.log(result)
+//       assert.strictEqual(result, 1)
+//     })
   
-    test('of many is calculated right', () => {
-      assert.strictEqual(average([1, 2, 3, 4, 5, 6]), 3.5)
-    })
+//     test('of many is calculated right', () => {
+//       assert.strictEqual(average([1, 2, 3, 4, 5, 6]), 3.5)
+//     })
   
-    test('of empty array is zero', () => {
-      assert.strictEqual(average([]), 0)
-    })
-  })
+//     test('of empty array is zero', () => {
+//       assert.strictEqual(average([]), 0)
+//     })
+//   })
