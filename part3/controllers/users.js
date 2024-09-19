40d7354cbbebafbd386f63ef1d362b803f11adc9
Lincoln Hoey Moore <johlinco@gmail.com>
2024-09-19 12:09:15 -0400

    setting up JWT

--- part3/controllers/users.js
+++ part3/controllers/users.js
@@ -1,28 +1,28 @@
 const bcrypt = require('bcrypt')
-const userRouter = require('express').Router()
+const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-userRouter.post('/', async (request, response) => {
-    const { username, name, password } = request.body
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
 
-    const user = new User({
-        username,
-        name, 
-        passwordHash
-    })
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
 
-    const savedUser = await user.save()
+  const savedUser = await user.save()
 
-    response.status(201).json(savedUser)
+  response.status(201).json(savedUser)
 })
 
-userRouter.get('/', async (request, response) => {
-    const users = await User.
-        find({}).populate('notes',  { content: 1, important: 1 })
-    response.json(users)
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+    .find({}).populate('notes', { content: 1, important: 1 })
+  response.json(users)
 })
 
-module.exports = userRouter
+module.exports = usersRouter
